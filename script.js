fd87ee6e3c63efdf0d87e878d1390dcce11fe1e6
Igor Rodrigues <120427686+Igualous@users.noreply.github.com>
2024-03-24 16:38:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,35 +8,27 @@ function verificarNomes() {
         // Se os nomes específicos foram inseridos, atualizar o conteúdo do HTML
         document.getElementById("resultado").innerHTML = "200%!";
         document.getElementById("instructions").innerHTML = "Parabéns! Você achou sua alma gêmea!";
-       
+        document.getElementById('container').style.backgroundImage = 'url("https://i.makeagif.com/media/10-21-2015/gyQ98G.gif")';
         let audio = new Audio('musica.mp3');
         audio.play();
-
-         document.getElementById('container').style.backgroundImage = 'url("https://i.makeagif.com/media/10-21-2015/gyQ98G.gif")';
     } else if (nome1 === "Igor" && nome2 === "Eduarda") {
         document.getElementById("resultado").innerHTML = "200%!";
         document.getElementById("instructions").innerHTML = "Parabéns! Você achou sua alma gêmea!";
-       
+        document.getElementById('container').style.backgroundImage = 'url("https://i.makeagif.com/media/10-21-2015/gyQ98G.gif")';
         let audio = new Audio('musica.mp3');
         audio.play();
-
-         document.getElementById('container').style.backgroundImage = 'url("https://i.makeagif.com/media/10-21-2015/gyQ98G.gif")';
     } else if  (nome1 === "eduarda" && nome2 === "igor") {
         document.getElementById("resultado").innerHTML = "200%!";
         document.getElementById("instructions").innerHTML = "Parabéns! Você achou sua alma gêmea!";
-       
+        document.getElementById('container').style.backgroundImage = 'url("https://i.makeagif.com/media/10-21-2015/gyQ98G.gif")';
         let audio = new Audio('musica.mp3');
         audio.play();
-
-         document.getElementById('container').style.backgroundImage = 'url("https://i.makeagif.com/media/10-21-2015/gyQ98G.gif")';
     } else if  (nome1 === "igor" && nome2 === "eduarda") {
         document.getElementById("resultado").innerHTML = "200%!";
         document.getElementById("instructions").innerHTML = "Parabéns! Você achou sua alma gêmea!";
-       
+        document.getElementById('container').style.backgroundImage = 'url("https://i.makeagif.com/media/10-21-2015/gyQ98G.gif")';
         let audio = new Audio('musica.mp3');
         audio.play();
-
-         document.getElementById('container').style.backgroundImage = 'url("https://i.makeagif.com/media/10-21-2015/gyQ98G.gif")';
     } else if (!nome1 || !nome2) {
         document.getElementById("instructions").innerHTML = "Por favor, forneça os dois nomes";
     }
